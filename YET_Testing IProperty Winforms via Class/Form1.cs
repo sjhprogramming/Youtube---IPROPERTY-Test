99beeba9e1e537c8a66b8d9a6aa9711eaac9c5b5
sjhprogramming <74790624+sjhprogramming@users.noreply.github.com>
2023-02-26 21:42:20 +0000

    Added Comments

--- YET_Testing IProperty Winforms via Class/Form1.cs
+++ YET_Testing IProperty Winforms via Class/Form1.cs
@@ -2,8 +2,8 @@ namespace YET_Testing_IProperty_Winforms_via_Class
 {
     public partial class Form1 : Form
     {
-        private Class1 _class1 = new();
-        Form2 newform = new Form2();
+        private Class1 _class1 = new();                     // Create a new object of our class!
+        Form2 newform = new Form2();                        // Create a new object of Form 2!
 
         public Form1()
         {
@@ -13,6 +13,11 @@ namespace YET_Testing_IProperty_Winforms_via_Class
             //Stop the user from playing with the form sizing......
             this.MaximumSize = new System.Drawing.Size(661, 333);
             this.MinimumSize = new System.Drawing.Size(661, 333);
+
+            // Set Max String length
+
+            TextBoxText.MaxLength = 20;
+
             #endregion
 
 
@@ -23,16 +28,31 @@ namespace YET_Testing_IProperty_Winforms_via_Class
 
         }
 
+        /// <summary>
+        /// Simple enough, lets exit the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
+        /// <summary>
+        /// Form load is not used in this app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
            
         }
 
+        /// <summary>
+        /// This Sets the TextToChange to "hello as a test"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             _class1.TextToChange = "Hello";
@@ -43,6 +63,11 @@ namespace YET_Testing_IProperty_Winforms_via_Class
 
         }
 
+        /// <summary>
+        /// Shows a message box upon a successful changeover
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e) => MessageBox.Show(_class1.TextToChange);
     }
 }
